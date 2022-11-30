1e6de669cf963dc0a95b4858b8335ffb98d24787
Kevin <kelvinngechu@gmail.com>
2022-11-30 09:03:48 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName="bob";
+function upperCaseCustomerName(){
+    return  customerName="BOB";
+}
+var bestCustomer;
+function setBestCustomer(){ 
+  return bestCustomer ='not bob'; 
+}
+
+
+function overwriteBestCustomer(){
+
+    return bestCustomer="maybe bob";
+}
+const leastFavoriteCustomer ="Kevin";
+ function changeLeastFavoriteCustomer(){
+    
+    return  leastFavoriteCustomer="Ngechu";
+ }
